1cae80e260e770dc18615c3c136dae69f9ff364c
Victor Zhabolenko <kenzbrug1337@yandex.ru>
2021-04-19 02:36:00 +0300

    fix: something

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
+require('dotenv').config();
 
 const config = {
   PORT: process.env.PORT || 3000,
-  JWT_SECRET: process.env.JWT_SECRET || "vEry-str0ng-sEcrEt-p@$$w0rd",
-  JWT_TTL: process.env.JWT_TTL || "7d",
+  JWT_SECRET: process.env.JWT_SECRET || 'vEry-str0ng-sEcrEt-p@$$w0rd',
+  JWT_TTL: process.env.JWT_TTL || '7d',
   MONGODB_SERVER:
-    process.env.MONGODB_SERVER || "mongodb://localhost:27017/bitfilmsdb",
+    process.env.MONGODB_SERVER || 'mongodb://localhost:27017/bitfilmsdb',
   MONGOOSE_CONFIG: {
     useNewUrlParser: true,
     useCreateIndex: true,
--- config/rateLimitConfig.js
+++ config/rateLimitConfig.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 const { RATE_LIMIT } = require('./errors');
 
 const limiterConfig = rateLimit({
-  windowMs: 15 * 60 * 1000, // количество запросов за 15 минут
+  windowMs: 15 * 60 * 100000, // количество запросов за 15 минут
   max: 100, // количество подключений с одного IP адреса за интервал времени
   message: RATE_LIMIT,
 });
