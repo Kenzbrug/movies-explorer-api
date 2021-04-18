a0efa817ce0ae4eb0018db51127d3e0c96dc0362
Victor Zhabolenko <kenzbrug1337@yandex.ru>
2021-04-19 01:23:07 +0300

    fix: default PORT

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const config = {
-  PORT: process.env.PORT || 3100,
+  PORT: process.env.PORT || 3000,
   JWT_SECRET: process.env.JWT_SECRET || 'vEry-str0ng-sEcrEt-p@$$w0rd',
   JWT_TTL: process.env.JWT_TTL || '7d',
   MONGODB_SERVER: process.env.MONGODB_SERVER || 'mongodb://localhost:27017/bitfilmsdb',
